a94c515ae111126dd74ce7e6fde60148498b03b4
Usenmfon <usenmfonuko@gmail.com>
2020-05-20 11:59:58 +0100

    added user info

--- src/GitTutorial/Program.cs
+++ src/GitTutorial/Program.cs
@@ -9,6 +9,12 @@ namespace GitTutorial
             Console.WriteLine("Hello World!");
             string name = "Usenmfon";
             Console.WriteLine("My name is " + name);
+            Console.WriteLine("Bio details");
+            string bankName = "Straford plc";
+            string contactNumber = "+1 7552341";
+            Console.WriteLine("Bio Details\n"+
+            "Bank: " + bankName +
+            "\nContact: " + contactNumber);
         }
     }
 }
