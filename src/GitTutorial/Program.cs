6a5d784a33a2f78718857a9982ad7e00c5d073dd
Usenmfon <usenmfonuko@gmail.com>
2020-05-20 11:49:31 +0100

    Added name

---
+++ src/GitTutorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "Usenmfon";
+            Console.WriteLine("My name is " + name);
+        }
+    }
+}
